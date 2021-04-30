a4942d0d810b1c8a3a242ebbf70c5b7992b14018
Subhra264 <chakrabortysubhradeep556@gmail.com>
2021-04-30 22:12:30 +0530

    Add test files for utils

---
+++ src/test/utils/encryption_utils/bcrypt_utils.test.ts
@@ -0,0 +1,15 @@
+
+
+describe('bcrypt_utils', () => {
+    test('should hash password', () => {
+        // TODO: test hashPassword function
+    });
+
+    test('should compare the given passwords without error', () => {
+        // TODO: test comparePassword function
+    });
+
+    test('should throw Unauthorized HttpError', () => {
+        // TODO: test comparePassword function
+    });
+});
---
+++ src/test/utils/jwt_utils/jwt_utils.test.ts
@@ -0,0 +1,9 @@
+// import signAccessToken from '../../../utils/jwt_utils/jwt_utils';
+
+describe('jwt_utils', () => {
+    test('should return signed access token', () => {
+        const userId = '234sddsjrejfpalj332';
+
+        // TODO: test the signAccessToken function
+    });
+});
