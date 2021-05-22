e306f45026a6be08c17fb6a10e36c4ef22c85fd6
Subhra264 <chakrabortysubhradeep556@gmail.com>
2021-05-22 14:40:34 +0530

    Add auth.middleware.ts

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Request, Response } from "express";
+import HttpErrors from "../errors/http-errors";
+import UserModel from "../models/User.model";
+import AuthenticatedRequest from "../utils/interfaces/AuthenticatedRequest";
+import { verifyToken } from "../utils/jwt_utils/jwt_utils";
+
+interface Payload extends Object {
+    userId: string;
+}
+
+export default async function authenticate(req: Request, res: Response, next: NextFunction) {
+    try {
+        let token = req.headers['authorization'];
+
+        if (!token) throw await HttpErrors.Unauthorized();
+        if (!token.startsWith('Bearer ')) throw await HttpErrors.Unauthorized();
+
+        token = token.replace('Bearer ', '');
+        const payload = await verifyToken(token);
+
+        const user = await UserModel.findById((payload as Payload).userId).exec();
+
+        if (!user) throw HttpErrors.Unauthorized();
+
+        (req as AuthenticatedRequest).user = user;
+        next(req);
+
+    } catch(err) {
+        next(err);
+    }
+}
--- src/utils/jwt_utils/jwt_utils.ts
+++ src/utils/jwt_utils/jwt_utils.ts
@@ -16,14 +16,33 @@ export async function signAccessToken(userId: string): Promise<string> {
             audience: userId
         };
 
-        if(!process.env.JWT_ACCESS_KEY) {
+        if (!process.env.JWT_ACCESS_KEY) {
             return reject(HttpErrors.ServerError());
         }
 
         jwt.sign(payload, process.env.JWT_ACCESS_KEY, options, (err: Error | null, token: string | undefined) => {
-            if(err || !token) return reject(HttpErrors.ServerError());
+            if (err || !token) return reject(HttpErrors.ServerError());
             resolve(token);
         });
     });
     
+}
+
+/**
+ * Verifies the given token and returns the payload
+ * @param token 
+ * @returns Promise that resolves to the payload
+ */
+export async function verifyToken(token: string): Promise<object> {
+
+    return new Promise((resolve, reject) => {
+        if (!process.env.JWT_ACCESS_KEY) {
+            return reject(HttpErrors.ServerError());
+        }
+
+        jwt.verify(token, process.env.JWT_ACCESS_KEY, (err, payload) => {
+            if (err || !payload) return reject(HttpErrors.Unauthorized());
+            resolve(payload); 
+        });
+    });
 }
