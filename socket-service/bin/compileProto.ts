dd52591003c4cf559b2fadcd04b46c4616d2f858
Subhra264 <chakrabortysubhradeep556@gmail.com>
2023-03-30 16:27:10 +0530

    socket: Add auth proto and compiler script

---
+++ socket-service/bin/compileProto.ts
@@ -0,0 +1,25 @@
+import path from 'path';
+import { execSync } from 'child_process';
+import rimraf from 'rimraf';
+
+const PROTO_DIR = path.join(__dirname, '../proto');
+const MODEL_DIR = path.join(__dirname, '../src/models');
+const PROTO_LOADER_PATH = path.join(
+  __dirname,
+  '../node_modules/.bin/proto-loader-gen-types',
+);
+
+rimraf.sync(`${MODEL_DIR}/*`, {
+  glob: { ignore: `${MODEL_DIR}/.gitignore` },
+});
+
+const protoConfig = [
+  '--enums=String',
+  '--longs=String',
+  `--outDir=${MODEL_DIR}`,
+  '--grpcLib=@grpc/grpc-js',
+  `proto/auth.proto`,
+];
+
+execSync(`${PROTO_LOADER_PATH} ${protoConfig.join(' ')}`);
+console.log(`> Proto models created: ${MODEL_DIR}`);
