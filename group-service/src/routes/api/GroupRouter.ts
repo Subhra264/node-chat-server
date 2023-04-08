e1117713cef401c580a5d8a3720df11837b0aaf2
Subhra264 <chakrabortysubhradeep556@gmail.com>
2023-04-08 23:09:03 +0530

    group: Rename Group.prisma to schema.prisma and add GroupMember schema

--- group-service/src/routes/api/GroupRouter.ts
+++ group-service/src/routes/api/GroupRouter.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response, Router } from 'express';
 
 const GroupRouter = Router();
 
+// TODO: Implement all the necessary routes
 GroupRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.json({
     status: 'success',
